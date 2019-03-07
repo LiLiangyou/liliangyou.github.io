0c75d1989d14c6629cecb3f7cf0dd2d10958fcfd
liliangyou <1149752461@qq.com>
2019-03-08 01:01:37 +0800

    video

---
+++ video/js/app-video.js
@@ -0,0 +1,41 @@
+var vm = new Vue({
+  el: "#app",
+  data: {
+    listOne: [{
+      id: "01",
+      text: "仿漫威片头（2D） | 12s",
+      video:"MAW.mp4",
+      img:"MAW.jpg",
+      time:"2018-03",
+      biao:"Ae/Pr/Ps/Au"
+    },{
+      id: "02",
+      text: "魅族Pro6s微解说 | 5min13s",
+      video:"pro6s.mp4",
+      img:"pro6s.jpg",
+      time:"2017-05",
+      biao:"Ae/Pr/Ps/Au"
+    },{
+      id: "03",
+      text: "团队开发 社交App演示视频 | 3min36s",
+      video:"db.mp4",
+      img:"db.jpg",
+      time:"2018-11",
+      biao:"Pr/Ps/Au"
+    },{
+      id: "04",
+      text: "Web协会宣传短视频 | 10s",
+      video:"Web1.mp4",
+      img:"Web1.jpg",
+      time:"2018-03",
+      biao:"Pr/Ps/Au"
+    },{
+      id: "03",
+      text: "Web协会宣传短视频（方形比例）| 10s",
+      video:"Web2.mp4",
+      img:"Web2.jpg",
+      time:"2018-03",
+      biao:"Pr/Ps/Au"
+    }]
+  }
+})
